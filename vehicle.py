c71a00291b132e66d586d8a948d14fb142089a37
Jawfalfara <Xelkomdata@outlook.com>
2025-01-08 18:42:53 +0100

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -5,7 +5,6 @@ warnings.filterwarnings('ignore')
 # Import necessary libraries
 import pandas as pd # type: ignore
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
